f70449663b0eb08b5de65ac45112e77516289e13
Thanos Psaridis <t.psaridis@blockachain.gr>
2017-10-24 16:28:09 +0300

    playing around with freqs

--- sample/src/main/java/com/thanosfisherman/freqgen/sample/MainActivity.java
+++ sample/src/main/java/com/thanosfisherman/freqgen/sample/MainActivity.java
@@ -1,15 +1,35 @@
 package com.thanosfisherman.freqgen.sample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
 {
+    final ContinuousBuzzer buzzer = new ContinuousBuzzer();
+    private int freq = 440;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        buzzer.setVolume(100);
+        buzzer.setPausePeriodSeconds(1);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.btn_play);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+
+                buzzer.setToneFreqInHz(freq);
+                freq += 50;
+            }
+
+        });
+        buzzer.play();
     }
 }
