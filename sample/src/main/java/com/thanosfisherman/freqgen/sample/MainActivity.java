8e1564f38cbe685965e60cde0b2814cd7acd106a
Thanos Psaridis <t.psaridis@blockachain.gr>
2017-10-24 17:18:23 +0300

    playing around with freqs and handlers

--- sample/src/main/java/com/thanosfisherman/freqgen/sample/MainActivity.java
+++ sample/src/main/java/com/thanosfisherman/freqgen/sample/MainActivity.java
@@ -1,35 +1,51 @@
 package com.thanosfisherman.freqgen.sample;
 
+import android.media.AudioManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
 {
-    final ContinuousBuzzer buzzer = new ContinuousBuzzer();
+    final ContinuousBuzzer tonePlayer = new ContinuousBuzzer();
     private int freq = 440;
+    private Handler mHandler = new Handler();
+    int vol = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
-        buzzer.setVolume(100);
-        buzzer.setPausePeriodSeconds(1);
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setVolumeControlStream(AudioManager.STREAM_MUSIC);
+        tonePlayer.setVolume(vol);
+        tonePlayer.setPausePeriodSeconds(2);
+        tonePlayer.setToneFreqInHz(freq);
         Button button = (Button) findViewById(R.id.btn_play);
-        button.setOnClickListener(new View.OnClickListener()
+        button.setOnTouchListener(new View.OnTouchListener()
         {
             @Override
-            public void onClick(View v)
+            public boolean onTouch(View v, MotionEvent event)
             {
+                //if (event.getAction() == MotionEvent.ACTION_DOWN)
 
-                buzzer.setToneFreqInHz(freq);
-                freq += 50;
+                return false;
             }
-
         });
-        buzzer.play();
+
+        mHandler.postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                tonePlayer.setVolume(vol += 5);
+                mHandler.postDelayed(this, 1000);
+            }
+        }, 1000);
+
+        tonePlayer.play();
     }
 }
